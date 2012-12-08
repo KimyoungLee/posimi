952e2a82f22431eae8b3f266a732a4010cb1126a
cei <jhp@cheoeumin.com>
2012-12-08 16:37:20 +0900

    08Dec

--- WebContent/app.js
+++ WebContent/app.js
@@ -3,32 +3,18 @@ Ext.require("Ext.TitleBar");
 Ext.require("Ext.MessageBox");
 
 var data = [
-    {
-    	date: 0,
-    	msg: "한글사랑 나라사랑",
-    	checkd: false
-    },
-    {
-    	date: 1354344478119,
-    	msg: "부모님사랑 나라사랑",
-    	checkd: false
-    },
-    {
-    	date: 1354344478119,
-    	msg: "할아부지사 나라사랑",
-    	checkd: false
-    },
-    {
-    	date: 1354344478119,
-    	msg: "할머니사랑 나라사랑",
-    	checkd: false
-    },
-    {
-    	date: 1354344478119,
-    	msg: "가족사랑 나라사랑",
-    	checkd: false
-    },
-];
+    	"0한글사랑 나라사랑한글사랑 나라사랑한글사랑\n나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"1부모님사랑 나라사랑, 한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"2할아부지사 나라사랑,한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"3할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"4할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"5할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"6할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"7할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"8할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"9할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑",
+    	"10할머니사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑한글사랑 나라사랑"
+    ];
 
 Ext.application({
     name: 'Posimi',
@@ -36,57 +22,57 @@ Ext.application({
     launch: function() {
         Ext.fly('appLoadingIndicator').destroy();
 
-        var listHtml = "<ul id='list' class='memory-list'></ul>";
+		var navi = Ext.create("Ext.TitleBar", {
+			docked : "top",
+			ui : "light",
+			title : "Posimi"
+		});
+		
 
-        var listComposit = function() {
-        	var i = 0,
-        	list = document.getElementById("list"),
-        	articles = list.getElementsByTagName("li");
-
-        	//기존리스트제거
-        	for(i = 0; i < articles.length; i++) list.removeChild(articles[i]);
-
-        	//새로 리스트 그리기
-        	for(var i = 0; i < data.length; i++) {
-        		var text = document.createTextNode(data[i].msg),
-        		li = document.createElement("li");
-        		
-        		li.appendChild(text);
-        		li.addEventListener("click", function(msg){
-        			return function(e) {
-            			Ext.Msg.alert("내용", msg);
-        			};
-        		}(data[i].msg), true);
-
-        		list.appendChild(li);
-        	}
-        };
-        
-    	var memory = Ext.create("Ext.Panel", {
-    		html: listHtml,
-    		scrollable: true
-    	});
-    	
     	var basePanel = Ext.create("Ext.Panel", {
     		layout: {
-    			type: "card",
+    			type: "vbox",
     			animation: {
     				type: "slide",
     				direction: "left"
     			}
     		},
+    		fullscreen: true,
     		items: [
-    		    memory
-    		]
+    		    navi
+    		],
+    		html: "<div class='wrap'><img src='./img/posimi.png' id='posimi'><div class='text-bound'><div id='text'></div></div></div> <div id='wrap'></div>"
     	});
-    	
+
     	Ext.Viewport.add(basePanel);
     	
+    	//after loaded
+    	var opened = false;
+    	var posimi = document.getElementById("posimi");
+    	var wrap = document.getElementById("wrap");
+    	wrap.addEventListener("click", function(e) {
+    		opened = !opened;
+    		
+    		console.log("check #1");
+    		
+    		if(opened) {
+    			text();
+    			posimi.src = "./img/posimi_open.png";
+    			document.getElementById("text").style.display = "block";
+    		} 
+    		else {
+    			posimi.src = "./img/posimi.png";
+    			document.getElementById("text").style.display = "none";
+    		}
+    	}, false);
     	
-
-    	//after on loaded
-    	(function(){
-    		listComposit();
-    	}());
+    	
+    	var text = function() {
+    		var num = Math.round(Math.random() * 10),
+    		text = document.getElementById("text");
+    		text.innerHTML = data[num];
+    		console.log("num : " + num);
+    		console.log(data[num]);
+    	};
     }
 });
